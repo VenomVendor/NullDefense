0f1fe55b4473bb0909f00c2cc59c4f018bb91c4f
VenomVendor <1086927+VenomVendor@users.noreply.github.com>
2024-06-10 21:51:51 -0400

    Migrated to version catalog
    Updated version for next release
    Updated dependencies version

--- version.gradle.kts
+++
@@ -1,9 +0,0 @@
-extra["major"] = 2
-extra["minor"] = 0
-extra["patch"] = 0
-
-val suffix = ""
-val version = "${extra["major"]}.${extra["minor"]}.${extra["patch"]}"
-
-extra["currentVersion"] = "$version$suffix"
-extra["group"] = "com.venomvendor"
